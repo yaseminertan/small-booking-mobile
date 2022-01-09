1518d54d077c7e3486f6dfdf15600c59e8e11c37
Yasemin Ertan <yasemin.ertan@despatchcloud.com>
2022-01-09 20:39:47 +0300

    add time boxes

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,22 @@
         <img class="banner-image"
              src="https://firebasestorage.googleapis.com/v0/b/dynamaze---pwa-1550221436823.appspot.com/o/public%2Fimg%2Fdeals%2Ffunfussball1.png?alt=media&token=448599c6-ffb7-43f4-afc9-dfb82ce426f0" 
         />
+         <div class="time-box-container">
+            <div 
+                :id="time.id"
+                :key="time" 
+                class="time-box" 
+                v-for="time in timeSlots"
+                @click="selectTime"
+            >
+                <span> {{ time.begin }} </span>
+                <span class="seperator"></span>
+                <span> {{ time.price.amount }} {{ time.price.currency }} </span>
+            </div>
+        </div>
+        <p class="text">Nur noch {{ ticketNumber }} Tickets vorhanden.</p>
+       
+       
      </div>
 </template>
 
@@ -13,7 +29,10 @@ import Header from '@/components/Header.vue';
 export default {
     name: 'App',
     components: {Header},
-    components: {
+    computed:{
+        ticketNumber(){
+            return 0;
+        },
     },
     data: function() {
         return {
@@ -44,7 +63,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss'>
 #app {
     font-family: Montserrat;
     -webkit-font-smoothing: antialiased;
@@ -69,5 +88,91 @@ body{
     border-radius: 20px 0px;
     width: 100%;
     height: auto;
+}
+.time-box-container{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 20px;
+    margin-bottom: 35px;
+
+    .time-box{
+        width: 70px;
+        height: 60px;
+        border-radius: 4px;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: #55C57A;
+        background:url('./assets/box-back.svg');
+        cursor: pointer;
+    }
+    .selected{
+        background: linear-gradient(225.08deg, #7ED56F 15.51%, #55C57A 100%);
+        color: #FFFFFF;
+        border:none;
+        .seperator{
+            border-top: 1px solid #fff;
+        }
+    }
+    .seperator{
+        height: 1;
+        width: 56px;
+        border-top: 1px solid #55C57A;
+    }
+}
+.text{
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+    color: #222222;
+}
+.ticket-select-container{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    .button{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 56px;
+        height: 40px;
+        background:url('./assets/button-back.svg');
+        border:none;
+        font-weight: 700;
+        font-size: 30px;
+        cursor: pointer;
+    }
+}
+.total-price{
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 22px;
+    color: #55C57A;
+    margin-top: 16px;
+}
+.buy-button{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto auto 37px;
+    width: 90%;
+    padding: 9px 0;
+    background: linear-gradient(87.6deg, #FF9232 -28.36%, #FAC162 104.8%);
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    color: #FFFFFF;
+    cursor: pointer;
 }
 </style>
