bacab1e5a981ba424e1bad17422a5b13253bf670
Yasemin Ertan <yasemin.ertan@despatchcloud.com>
2022-01-09 20:18:53 +0300

    add header and banner image

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-    <p>Data:</p>
-    {{ JSON.stringify(timeSlots, null, 4) }}
+     <div class="container">
+        <Header />
+        <img class="banner-image"
+             src="https://firebasestorage.googleapis.com/v0/b/dynamaze---pwa-1550221436823.appspot.com/o/public%2Fimg%2Fdeals%2Ffunfussball1.png?alt=media&token=448599c6-ffb7-43f4-afc9-dfb82ce426f0" 
+        />
+     </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue';
+
 export default {
     name: 'App',
+    components: {Header},
     components: {
     },
     data: function() {
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <img src="@/assets/dynamaze_logo.png"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
